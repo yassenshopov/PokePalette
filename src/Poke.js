c1fd0856e8b6590677214a215b80af029a1516a5
Yassen Shopov <yassen.shopov@nuvei.com>
2024-03-13 13:02:24 +0000

    Add Firebase configuration using environment variables

--- src/Poke.js
+++ src/Poke.js
@@ -24,7 +24,12 @@ import {
 
 // FIREBASE CONFIGURATION
 const firebaseConfig = {
-
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 function createFirebaseApp(creds) {
